e4b361e141d930261255cd00ee92b2618ea44165
Pedro Velez <pvelezn@amazon.com>
2024-10-02 10:48:20 -0600

    Cambiar texto y justificarlo

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -8,10 +8,12 @@ function Home() {
       <h1 className="main-heading">Boda de Max y Mafer</h1>
       <CustomCarousel/>
       <div className="">
-        <h2> Cómo funciona?</h2>
-        <p>
-        "Estamos emocionados de compartir con ustedes un día tan especial en nuestras vidas: ¡nuestra boda! 
+        <h2> ¿Cómo funciona?</h2>
+        <p style={{textAlign: 'justify', textJustify: 'inter-word'}}>
+        Estamos emocionados de compartir con ustedes un día tan especial en nuestras vidas: ¡nuestra boda! 
         Para que puedan ser parte de cada momento y recordar juntos esta fecha, hemos creado una manera divertida y sencilla de compartir sus fotos y videos.
+        </p>
+        <p style={{textAlign: 'justify', textJustify: 'inter-word'}}>
         En esta página podrás subir tus mejores momentos durante la celebración. Tu aportación permitirá que todos disfrutemos este inolvidable día a través de diferentes perspectivas.
         ¡No podemos esperar a ver todas las sonrisas, bailes y momentos ! 
         Gracias por ser parte de nuestra historia. 
